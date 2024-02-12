322717afa4cd53dd00f9dad57db412b0f903e769
vandrep <andrepinedacunha@gmail.com>
2024-02-12 17:41:38 -0300

    fix: ajustar script com valores esperados

--- luizpais/sql/script.sql
+++ luizpais/sql/script.sql
@@ -25,8 +25,8 @@ alter sequence public.contacorrente_seq owner to rinha;
 alter sequence public.movimento_seq owner to rinha;
 
 INSERT INTO public.contacorrente (id, nome, saldo, limite)
-VALUES (1, 'odorico paraguaçu', 0, 80000),
-       (2, 'irmas cajazeira', 0, 10000000),
-       (3, 'coronel nepomuceno', 0, 500000),
-       (4, 'dirceu borboleta', 0, 1000000),
-       (5, 'zeca diabo', 0, 100000);
+VALUES (1, 'odorico paraguaçu', 0, 100000),
+       (2, 'irmas cajazeira', 0, 80000),
+       (3, 'coronel nepomuceno', 0, 1000000),
+       (4, 'dirceu borboleta', 0, 10000000),
+       (5, 'zeca diabo', 0, 500000);
